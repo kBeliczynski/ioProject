50afabe62559f1fd497f2f199c0656a5d3f973bd
ondondil <brainlesstail@gmail.com>
2020-05-08 13:56:31 +0200

    Implementacja obsługi plików wideo

--- wideo.cpp
+++ wideo.cpp
@@ -2,5 +2,32 @@
 
 Wideo::Wideo()
 {
+    tytul = "";
+    zawartosc = "";
+}
 
+Wideo::Wideo(const std::string &tytul, const std::string &zawartosc)
+{
+    this->tytul = tytul;
+    this->zawartosc = zawartosc;
+}
+
+std::string Wideo::getZawartosc() const
+{
+    return zawartosc;
+}
+
+void Wideo::setZawartosc(const std::string &value)
+{
+    zawartosc = value;
+}
+
+std::string Wideo::getTytul() const
+{
+    return tytul;
+}
+
+void Wideo::setTytul(const std::string &value)
+{
+    tytul = value;
 }
--- wideo.h
+++ wideo.h
@@ -1,11 +1,19 @@
 #ifndef WIDEO_H
 #define WIDEO_H
 
+#include <string>
 
 class Wideo
 {
+    std::string tytul;
+    std::string zawartosc;
 public:
     Wideo();
+    Wideo(const std::string &tytul, const std::string &zawartosc);
+    std::string getTytul() const;
+    void setTytul(const std::string &value);
+    std::string getZawartosc() const;
+    void setZawartosc(const std::string &value);
 };
 
 #endif // WIDEO_H
